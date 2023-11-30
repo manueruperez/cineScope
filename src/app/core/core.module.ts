9198f457765f52e67b28cd6789d06127d3a49aee
manueru <juanperez@gcflatino.com>
2023-11-30 15:59:45 -0500

    feat(layout ): arquitecture for layout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    CoreRoutingModule
-  ]
+  declarations: [HeaderComponent, FooterComponent, MainLayoutComponent],
+  imports: [CommonModule, CoreRoutingModule],
+  exports: [MainLayoutComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
