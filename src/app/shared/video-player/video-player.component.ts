da6212fb8e5bb61c618adfa47186e70e042093ce
manueru <juanperez@gcflatino.com>
2023-12-03 21:46:20 -0500

    feat(videoplayer):stop iframe reload on changes

--- src/app/shared/video-player/video-player.component.ts
+++ src/app/shared/video-player/video-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
@@ -6,10 +6,19 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   templateUrl: './video-player.component.html',
   styleUrls: ['./video-player.component.scss'],
 })
-export class VideoPlayerComponent {
+export class VideoPlayerComponent implements OnChanges {
   @Input() videoUrl!: string;
+  safeUrl!: SafeResourceUrl;
+
   constructor(private sanitizer: DomSanitizer) {}
-  getSafeTrailerUrl(url: string): SafeResourceUrl {
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['videoUrl'] && this.videoUrl) {
+      this.safeUrl = this.getSafeTrailerUrl(this.videoUrl);
+    }
+  }
+
+  private getSafeTrailerUrl(url: string): SafeResourceUrl {
     const videoId = url.split('v=')[1];
     const embedUrl = `https://www.youtube.com/embed/${videoId}`;
     return this.sanitizer.bypassSecurityTrustResourceUrl(embedUrl);
