22971c96d440f83dd7603a4eeab122879fd1968d
manueru <juanperez@gcflatino.com>
2023-12-03 20:52:36 -0500

    feat(movei-details): watchlist opction

--- src/app/movies/movies-details/movies-details.component.ts
+++ src/app/movies/movies-details/movies-details.component.ts
@@ -3,6 +3,7 @@ import { Movie } from '../../models/movie.model';
 import { MovieDataService } from '../../shared/movie-data-state.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subject, takeUntil } from 'rxjs';
+import { WatchlistService } from '../../shared/watchlist.service';
 
 @Component({
   selector: 'app-movies-details',
@@ -12,9 +13,11 @@ import { Subject, takeUntil } from 'rxjs';
 export class MoviesDetailsComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
   movie: Movie | null = null;
+  isInWatchList: boolean = false;
 
   constructor(
     private movieDataService: MovieDataService,
+    private watchlistService: WatchlistService,
     private router: Router,
     private route: ActivatedRoute
   ) {}
@@ -23,10 +26,30 @@ export class MoviesDetailsComponent implements OnInit, OnDestroy {
       const movieId = +params.get('id')!;
       this.loadMovieDetails(movieId);
     });
+
+    this.watchlistService.watchList$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((watchList) => {
+        if (this.movie) {
+          this.isInWatchList = watchList.some((m) => m.id === this.movie!.id);
+        }
+      });
   }
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  onAddToWatchlist() {
+    if (this.movie) {
+      this.watchlistService.addToWatchList(this.movie);
+    }
+  }
+
+  onRemoveToWatchlist() {
+    if (this.movie) {
+      this.watchlistService.removeFromWatchList(this.movie.id);
+    }
   }
   private loadMovieDetails(movieId: number) {
     const movie = this.movieDataService.getMovieById(movieId);
